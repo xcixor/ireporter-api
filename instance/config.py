f3f3414d486914312683bd1a230355be81ff2020
Peter <pndungu54@gmail.com>
2018-12-06 16:56:52 +0300

    Chore Set up database
    -Add app configurations
    
    -Add app dependencies
    
    -Add app initialization and run scripts
    
    -Add database set up script
    
    -Add app installation to readme.md
    
    [Finishes #162475977]

---
+++ instance/config.py
@@ -0,0 +1,51 @@
+"""Contains configurations for the app."""
+
+import os
+
+
+class Config:
+    """Contains the basic settings for all configurations."""
+
+    debug = False
+    SECRET_KEY = os.urandom(30)
+
+
+class Development(Config):
+    """Contains configurations to be used by developer."""
+
+    DEBUG = True
+    db = {
+        "dbname": os.getenv('DATABASE_NAME'),
+        "user": os.getenv('USER'),
+        "password": os.getenv('PASSWORD'),
+        "host": os.getenv('HOST'),
+        "port": os.getenv('POSTRGRESPORT')
+    }
+
+
+class Testing(Config):
+    """Contains configurations for testing."""
+
+    TESTING = True
+    db = {
+        "dbname": os.getenv('test_dbname'),
+        "user": os.getenv('user'),
+        "password": os.getenv('password'),
+        "host": os.getenv('host'),
+        "port": os.getenv('postgresport')
+    }
+
+
+class Production(Config):
+    """Contains configurations for production setting."""
+
+    DEBUG = False
+
+
+# Register the configurations
+config = {
+    'development': Development,
+    'testing': Testing,
+    'production': Production,
+    'default': Development
+}
